1218ebdd8f1a3198552bf271b2bae1ee14cdeb6d
Macwan Robert <macwanrobert04gmail.com>
2022-05-29 21:30:43 +0530

    Updates

---
+++ precache-manifest.7cabb27fa1ab0867da26956084434ce2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9801ba5a4fb6b0d49165",
+    "url": "/time-tracking-dashboard/static/css/main.53fb11f8.chunk.css"
+  },
+  {
+    "revision": "9801ba5a4fb6b0d49165",
+    "url": "/time-tracking-dashboard/static/js/main.9801ba5a.chunk.js"
+  },
+  {
+    "revision": "17843e044b530d0a3e6b",
+    "url": "/time-tracking-dashboard/static/js/1.17843e04.chunk.js"
+  },
+  {
+    "revision": "b196d74f33503cfdbd5d",
+    "url": "/time-tracking-dashboard/static/js/runtime~main.b196d74f.js"
+  },
+  {
+    "revision": "a27ff7cf343a12be1c97eb1f29dbd51d",
+    "url": "/time-tracking-dashboard/index.html"
+  }
+];
